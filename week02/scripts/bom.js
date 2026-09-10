ca24c9b3962508eec9a734d17816ef8a16d18604
vilcasud-lab <vilcasud@gmail.com>
2026-09-10 16:57:21 -0500

    bom week 2

---
+++ week02/scripts/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list"); // Usamos el ID del HTML
+
+button.addEventListener("click", function () {
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = "";
+        input.focus();
+    } else {
+        input.focus();
+    }
+});
+
